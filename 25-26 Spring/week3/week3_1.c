43849dc780cf264be53756ed3dfaf4d6180c42ef
Batuhan Karadağ <52932122+batukar@users.noreply.github.com>
2026-02-19 18:25:45 +0300

    week3_inserted

---
+++ 25-26 Spring/week3/week3_1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// satir-sutun tanimlamasi
+#define ROWS 3
+#define COLS 3
+
+// global iki boyutlu dizi (matris) tanimlamasi
+// baslangic durumunda hepsi bos
+// target in amaci: toplam, carpim gibi matris sonuclarinin saklanmasi
+int arrayA[ROWS][COLS] = {};
+int arrayB[ROWS][COLS] = {};
+int target[ROWS][COLS] = {};
+
+// fonksiyon prototipleri
+void randomValueGenerator(int arr[ROWS][COLS]);
+
+void showMatrix(int arr[ROWS][COLS]);
+
+int main(){
+    srand(time(NULL));
+
+    printf("A matrisi:\n");
+    randomValueGenerator(arrayA);
+
+    printf("\nB matrisi:\n");
+    randomValueGenerator(arrayB);
+}
+
+void randomValueGenerator(int arr[ROWS][COLS]){
+    for (int i = 0; i < ROWS; i++){
+        for (int j = 0; j < COLS; j++){
+            arr[i][j] = rand() % 6;
+        }
+    }
+    showMatrix(arr);
+}
+
+void showMatrix(int arr[ROWS][COLS]){
+    for (int i = 0; i < ROWS; i++){
+        for (int j = 0; j < COLS; j++){
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
