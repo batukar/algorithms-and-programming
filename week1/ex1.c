c452b6f227c8273b654a1a92ddd49b1dd6f6760a
Batuhan Karadağ <52932122+batukar@users.noreply.github.com>
2026-09-25 22:39:13 +0300

    edited

---
+++ week1/ex1.c
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+#include <stdio.h>
+
+int main(){
+    int tamSayi = 5; // istediğim şeyi yazıcammmm
+    int a = 500;
+    // a = 100;
+    int sonuc = a + tamSayi;
+
+
+
+    printf("birinci sayi : %d\n", tamSayi);
+    printf("ikinci sayi: %d\n", a);
+    printf("ikisinin toplami = %d\n", sonuc);
+
+    printf("%d + %d = %d\t", tamSayi, a, sonuc);
+
+
+
+
+
+}
